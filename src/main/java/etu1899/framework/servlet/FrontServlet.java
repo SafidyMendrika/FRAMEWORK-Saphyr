4246ca27775e4974425a022d60df6a35bd874abb
SafidyMendrika <mendrikarazafimalaza@gmail.com>
2023-03-07 10:20:13 +0300

    pushing sprint 2

--- src/main/java/etu1899/framework/servlet/FrontServlet.java
+++ src/main/java/etu1899/framework/servlet/FrontServlet.java
@@ -1,14 +1,18 @@
 package etu1899.framework.servlet;
 
+import etu1899.framework.Mapping;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 import etu1899.framework.utility.Util;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 @WebServlet(name = "FrontServlet", value = "/")
 public class FrontServlet extends HttpServlet {
+    HashMap<String, Mapping> mappingUrls;
+
 
     protected void processRequest(HttpServletRequest request,HttpServletResponse response){
         try {
